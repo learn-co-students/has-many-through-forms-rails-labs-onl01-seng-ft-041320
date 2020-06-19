bccb0c99cdb422ba6b31ea40b57f1e8221e955a9
markcascardi <mcascardi@gmail.com>
2020-06-19 14:06:24 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+post1 = Post.create([{ title: 'post 1' }, { content: 'a fake post' }])
+post2 = Post.create([{ title: 'post 2' }, { content: 'a faker post' }])
+post3 = Post.create([{ title: 'post 3' }, { content: 'a fakiest post' }])
+
+user = User.create([{ username: 'Mark' }, { email: 'mcascardi@gmail.com' }])
+
+category1 = Category.create([{ name: 'opinions' }])
+
+
+
+#   Mayor.create(name: 'Emanuel', city: cities.first)
