1e96757473bd784f4362a3c68909edee16eb5060
Dhrubot <dhrubo.dh@gmail.com>
2020-06-24 19:40:46 -0400

    Done.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,4 +3,23 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
   end
+
+  def new 
+    @category = Category.new
+  end
+
+  def index
+    @categories = Category.all
+  end
+
+  def create
+    category = Category.create(category_params)
+    redirect_to category
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
